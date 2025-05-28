13fac6a67c9ccb589d9b32de06d88d4c61177b6e
DD1827-DHIRAJ <ahlawatjaat537@gmail.com>
2025-05-28 23:35:50 +0530

    Update login_auto.py

--- login_auto.py
+++ login_auto.py
@@ -1,22 +1,13 @@
-
 from kiteconnect import KiteConnect
 import os
-import time
-import webbrowser
 
 def auto_login():
-    api_key = "n0blt8u5o0o0muzk"
-    api_secret = "3xcvr258l619a552cp4lju8q676iizel"
+    api_key = os.environ.get('KITE_API_KEY')
+    api_secret = os.environ.get('KITE_API_SECRET')
+    request_token = os.environ.get('KITE_REQUEST_TOKEN')  # Now taken from environment
 
     kite = KiteConnect(api_key=api_key)
-
-    print("🔗 Open login URL in browser...")
-    url = kite.login_url()
-    webbrowser.open(url)
-
-    request_token = input("Paste request token from URL here: ")
     data = kite.generate_session(request_token, api_secret=api_secret)
     kite.set_access_token(data["access_token"])
-
     print("✅ Login successful. Access token set.")
     return kite
