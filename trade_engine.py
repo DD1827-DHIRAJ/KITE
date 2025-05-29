03ddeba031329ab901320484514a607e18853386
DD1827-DHIRAJ <ahlawatjaat537@gmail.com>
2025-05-29 08:47:35 +0530

    Delete trade_engine.py

--- trade_engine.py
+++
@@ -1,6 +0,0 @@
-def run_smc_strategy(kite):
-    print("🚀 Running Normal Smart Money Concept Trade Logic")
-    # Placeholder for SMC trade logic
-    # Example:
-    # data = kite.ltp("NSE:NIFTY 50")
-    # print(data)
