e074009c447770f7bf928446abd8e8ec03200496
DD1827-DHIRAJ <ahlawatjaat537@gmail.com>
2025-05-29 12:21:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from login_auto import auto_login
 from trade_engine import run_trade_bot
 
-if __name__ == "__main__":
+login_attempted = False
+kite = None
+
+if not login_attempted:
     kite = auto_login()
-    if kite:
-        print("✅ Bot is now ready to trade.")
-        run_trade_bot(kite)
-    else:
-        print("❌ Exiting due to failed login.")
+    login_attempted = True
+
+if kite:
+    print("✅ Bot is now ready to trade.")
+    run_trade_bot(kite)
+else:
+    print("❌ Exiting due to failed login.")
