9993d814cf8cf2b5679719d6a1828409ccee60e8
DD1827-DHIRAJ <ahlawatjaat537@gmail.com>
2025-05-29 12:42:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,13 @@
 from login_auto import auto_login
 from trade_engine import run_trade_bot
 
-# Only login once when service starts
+# Run once and exit cleanly after 1 login attempt
 if __name__ == '__main__':
-    try:
-        print("🔁 Starting Auto Login")
-        kite = auto_login()
-        
-        if kite:
-            print("✅ Bot is now ready to trade.")
-            run_trade_bot(kite)
-        else:
-            print("❌ Login failed or token expired. Exiting.")
-            
-    except Exception as e:
-        print(f"❌ Unexpected error: {str(e)}")
+    print("🚀 Starting Auto Login")
+    kite = auto_login()
+
+    if kite:
+        print("✅ Bot is now ready to trade.")
+        run_trade_bot(kite)
+    else:
+        print("❌ Login failed. Exiting.")
