106390be4cb52974596d97ced25a9f364691a185
DD1827-DHIRAJ <ahlawatjaat537@gmail.com>
2025-05-29 11:55:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from login_auto import auto_login
 
-kite = auto_login()
+try:
+    kite = auto_login()
+    print("📈 Bot is now ready to trade.")
+except Exception as e:
+    print("🛑 Login failed or already logged in. Skipping re-login.")
+    exit()
 
 if kite:
     print("馃搱 Bot is now ready to trade.")
